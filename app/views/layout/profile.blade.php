9339e89f800f55632ec43fd8231b542bd022b197
ai376928387 <beini.gao@gmail.com>
2015-07-02 23:13:53 +1200

    profile modify

--- app/views/layout/profile.blade.php
+++ app/views/layout/profile.blade.php
@@ -4,15 +4,15 @@
 			<h2 class="title">
 				About me
 			</h2>
-		</div>
-		<div class="widget-content">
-			<div class="profile-img">
-				{{ HTML::image('images/img-profile.png','profile',array(
-					'class'=>'img-circle',
-					'width'=>'120'
-				)) }}
+			<div class="widget-content">
+				<div class="profile-img">
+					{{ HTML::image('images/img-profile.png','profile',array(
+						'class'=>'img-circle',
+						'width'=>'120'
+					)) }}
+				</div>
 			</div>
-		<div class="clearfix"></div>
 		</div>
 	</div>
+	<div class="clearfix"></div>
 </div>
