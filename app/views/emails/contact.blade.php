54ed53658415dd57ff95f17d3c1512c34320196c
ai376928387 <beini.gao@gmail.com>
2015-11-29 22:50:37 +1300

    clean workbrench

--- app/views/emails/contact.blade.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <body>
-    	Hi wicky:<br />
-
-    	{{$fromName}} sent you a message from your portfolio website: <br />
-    	Email: {{$fromEmail}}<br/>
-        Message: {{$data}} <br />
-    </body>
-</html>
--- app/views/layout/foot.blade.php
+++
@@ -1,5 +0,0 @@
-<div id="footer">
-	<p class="footer-link">
-	  @Powered by Wicky Gao 2015. Build with Laravel 4.2
-	</p>
-</div>
